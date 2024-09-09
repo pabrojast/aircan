4fae6acdfb20036b76bd5f53f7e8228f19e696af
Pablo Ignacio Rojas Torres <pablo.ignacio.rt@gmail.com>
2024-09-09 14:04:19 -0300

    Update dag_korea_download.py

--- aircan/dags/dag_korea_download.py
+++ aircan/dags/dag_korea_download.py
@@ -22,7 +22,7 @@ APIdev = Variable.get("APIDEV")
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 9, 9),
+    'start_date': datetime(2024, 9, 9),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
