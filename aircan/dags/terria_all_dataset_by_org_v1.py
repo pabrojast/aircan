1e8d49f7ca95bf613f948fbfd25d513217be318d
Pablo Ignacio Rojas Torres <pablo.ignacio.rt@gmail.com>
2024-11-13 17:29:58 -0300

    Update terria_all_dataset_by_org_v1.py

--- aircan/dags/terria_all_dataset_by_org_v1.py
+++ aircan/dags/terria_all_dataset_by_org_v1.py
@@ -743,9 +743,9 @@ def generate_and_upload_catalog_by_tag():
         upload_ckan(tag_filename, entity_name=tag_name, entity_type='tag')
 
     # Save and upload consolidated catalog
-    write_catalog_file(convert_sets_to_lists(catalog), "IHP-WINS_tags.json")
-    print("Consolidated catalog by tags saved to: IHP-WINS_tags.json")
-    upload_ckan("IHP-WINS_tags.json")
+    #write_catalog_file(convert_sets_to_lists(catalog), "IHP-WINS_tags.json")
+    #print("Consolidated catalog by tags saved to: IHP-WINS_tags.json")
+    #upload_ckan("IHP-WINS_tags.json")
 
 # ------------------------------
 # Airflow DAG Configuration
