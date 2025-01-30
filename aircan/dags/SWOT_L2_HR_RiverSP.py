d69af9c21aece7331099490f16feb7ce1143f2d4
Pablo Ignacio Rojas Torres <pablo.ignacio.rt@gmail.com>
2025-01-30 01:24:29 -0300

    Create SWOT_L2_HR_RiverSP.py

---
+++ aircan/dags/SWOT_L2_HR_RiverSP.py
@@ -0,0 +1,150 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.models import Variable
+import subprocess
+import sys
+import os
+import requests
+import json
+from datetime import datetime, timedelta
+from time import sleep
+###config
+APIdev = Variable.get("APIDEV")
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 1, 30),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+# Retry function for resilience
+def retry(func, max_attempts=3, sleep_time=5):
+    attempts = 0
+    while attempts < max_attempts:
+        try:
+            return func()
+        except Exception as e:
+            print(f"Error: {e}. Retrying...")
+            sleep(sleep_time)
+            attempts += 1
+    raise Exception(f"Failed after {max_attempts} attempts.")
+
+
+
+
+def check_file_exists_in_api(name):
+    api_url = "https://data.dev-wins.com/api/3/action/package_show"
+    params = {
+        "id": "surface-water-and-ocean-topography-of-the-dnipro-river-in-ukraine"
+    }
+    
+    try:
+        response = requests.get(api_url, params=params)
+        response.raise_for_status()
+        data = response.json()
+        
+        if data["success"]:
+            resources = data["result"]["resources"]
+            name_lower = name.lower()  # Convert input filename to lowercase
+            for resource in resources:
+                resource_url = resource["url"].lower()  # Convert URL to lowercase
+                if resource_url.endswith(name_lower):
+                    print("Skipping file", name)
+                    last_modified = datetime.strptime(resource["last_modified"], "%Y-%m-%dT%H:%M:%S.%f")
+                    return True, last_modified
+        
+        return False, None
+        
+    except requests.exceptions.RequestException as e:
+        print(f"Error al consultar la API: {str(e)}")
+        return False, None
+
+def download_swot_data():    
+    command = [
+        'podaac-data-subscriber',
+        '-c', 'SWOT_L2_HR_RiverSP_2.0',
+        '-d', './data',
+        '--start-date', '2024-12-01T00:00:00Z',
+        '--end-date', '2025-31-01T00:00:00Z',
+        '-e', '.zip',
+        '-b', '22,44,40,52'
+    ]
+    
+    try:
+        result = subprocess.run(command, check=True, capture_output=True, text=True)
+        print("Descarga exitosa!")
+        print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print("Error durante la descarga:")
+        print(e.stderr)
+        sys.exit(1)
+
+def upload_ckan(package_id="surface-water-and-ocean-topography-of-the-dnipro-river-in-ukraine",
+                url='https://data.dev-wins.com/api/3/action/resource_create'):
+    """
+    Upload all SWOT data files from the data directory to CKAN platform
+    """
+    data_dir = "./data"
+    
+    # Verificar si el directorio existe
+    if not os.path.exists(data_dir):
+        print(f"El directorio {data_dir} no existe")
+        return
+    
+    # Procesar cada archivo en el directorio
+    for filename in os.listdir(data_dir):
+        file_path = os.path.join(data_dir, filename)
+        exists, last_modified = check_file_exists_in_api(filename)
+        if exists:
+            print(f"El archivo ya existe en la API (última modificación: {last_modified})")
+            print("Cancelando la subida...")
+            continue
+        # Solo procesar archivos, no directorios
+        if os.path.isfile(file_path):
+            try:
+                files = {'upload': open(file_path, 'rb')}
+                headers = {"API-Key": APIdev}
+                
+                data_dict = {
+                    'package_id': package_id,
+                    'name': filename,
+                    'description': f'SWOT data file: {filename}',
+                    'format': 'shp'  
+                }
+                
+                # POST request
+                response = requests.post(url, headers=headers, data=data_dict, files=files)
+                
+                # Logging
+                print(f"Subiendo archivo: {filename}")
+                print("Status Code:", response.status_code)
+                print("JSON Response:", response.json())
+                
+            except Exception as e:
+                print(f"Error al subir {filename}: {str(e)}")
+            finally:
+                files['upload'].close()
+
+# Define the hourly DAG
+hourly_dag = DAG(
+    'Nasa_hourly_data',
+    default_args=default_args,
+    description='A DAG to fetch hourly nasa SWOT_L2_HR_RiverSP_2.0',
+    schedule_interval="0 */3 * * *",
+    catchup=False,
+)
+# Hourly tasks
+fetch_hourly_meteo = PythonOperator(
+    task_id='download_swot_data',
+    python_callable=download_swot_data,
+    dag=hourly_dag,
+)
+# Añadir la tarea de upload al DAG
+upload_swot = PythonOperator(
+    task_id='upload_swot_data',
+    python_callable=upload_ckan,
+    dag=hourly_dag,
+)
+# Definir el orden de las tareas
+fetch_hourly_meteo >> upload_swot
